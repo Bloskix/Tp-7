6684d949cd06ee162fae07c467585213f6d5d096
Pablo Coello <pablofranciscocoello@gmail.com>
2022-06-06 12:38:10 -0300

    Adicion del punto 5

---
+++ punto_5.py
@@ -0,0 +1,28 @@
+def consecutive(list,s_list):
+    for i in range(len(list)):
+        for j in range(len(s_list)):
+            if (list[i] == s_list[j] and list[i + 1] == s_list[j + 1]):
+                condition = "True"
+                break
+            else:
+                condition = "False"
+            return condition
+print("Select the first consecutive number")
+num1 = int(input())
+print("Select the second consecutive number")
+num2 = int(input())
+consecutive_numbers = []
+consecutive_numbers.append(num1,num2)
+list_numbers = []
+print("Add a number:")
+num = int(input())
+list_numbers.append(num)
+print("Do you want to add another number?\n1.Ye\n2.No")
+answer = int(input())
+while answer == 1:
+    list_numbers = []
+    print("Add a number:")
+    num = int(input())
+    list_numbers.append(num)
+    print("Do you want to add another number?\n1.Yes\n2.No")
+    answer = int(input())
